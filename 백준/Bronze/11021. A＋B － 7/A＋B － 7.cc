a474cdc467fbe1979054e5e3951065ea42b7d45d
Belokan2522 <135136939+Belokan2522@users.noreply.github.com>
2023-12-30 11:21:57 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+    int T = 0;
+    scanf("%d",&T);
+    int A[T], B[T];
+
+    for(int i = 1; i<=T; i++){
+        scanf("%d %d", &A[i], &B[i]);
+    }
+    for(int x = 1; x<=T; x++){
+        printf("Case #%d: %d\n",x ,A[x]+B[x]);
+    }
+    return 0;
+}
